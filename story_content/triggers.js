846017932bdca14edc866b7878612f44be4dfc5e
rashleo <99816890+rashleo@users.noreply.github.com>
2024-12-24 16:58:21 +0530

    Add files via upload
    Updated version

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,71 +2,32 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6aJBo4vAnMU":
+      case "5wxqAsh2k5K":
         Script1();
         break;
-      case "5frcFhD3Y0M":
+      case "6FTg6s2jfN8":
         Script2();
         break;
-      case "5wCzeZzPJqj":
+      case "6cyOLHiC00h":
         Script3();
         break;
-      case "6TI6MEmXKyy":
+      case "6Mn2Jxv1u4x":
         Script4();
         break;
-      case "5w0BrlpVVLm":
+      case "5yCg0kCZgu6":
         Script5();
         break;
-      case "5X9Fw925r5T":
+      case "6O24vUvfmgR":
         Script6();
         break;
-      case "69yIaQU8Hle":
+      case "6l9bIlCBkWl":
         Script7();
         break;
-      case "6N8FBscysXp":
+      case "6gdAn3OEUYP":
         Script8();
         break;
-      case "5vmZZ8KBlQz":
+      case "5tikASyTyBe":
         Script9();
-        break;
-      case "66K1A2jBhYk":
-        Script10();
-        break;
-      case "5i8cZxBk8AF":
-        Script11();
-        break;
-      case "5Z1UM961Zcm":
-        Script12();
-        break;
-      case "6hSvZAw3mVQ":
-        Script13();
-        break;
-      case "6K86o74YhA0":
-        Script14();
-        break;
-      case "5d9kRyrZOrw":
-        Script15();
-        break;
-      case "6JFbkpFphZT":
-        Script16();
-        break;
-      case "6NUSzWBd88V":
-        Script17();
-        break;
-      case "5pbtsyOeHY7":
-        Script18();
-        break;
-      case "6mouDO5ycHn":
-        Script19();
-        break;
-      case "5lSdJZTYaSS":
-        Script20();
-        break;
-      case "63qWnlf9DbR":
-        Script21();
-        break;
-      case "6ak5prHq8Jb":
-        Script22();
         break;
   }
 }
@@ -78,4 +39,154 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5lHeL3mpcmz');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6nyxYl5ntb6';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('6Xodz24XRoU');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6gzP1to6qp9';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script3 = function()
+{
+  player.once(() => {
+const target = object('6J3WRpvnS1L');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6iSSYGuHXmB';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script4 = function()
+{
+  player.once(() => {
+const target = object('6gsGiIf1baB');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6iSSYGuHXmB';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script5 = function()
+{
+  player.once(() => {
+const target = object('6MSX7Nf3PTa');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5xyFxTbL9t0';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script6 = function()
+{
+  player.once(() => {
+const target = object('6TlelDAa79b');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5q1n9D0ZATV';
+const shakeAmount = 5;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
 };
